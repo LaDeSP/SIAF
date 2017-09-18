fe14c3230f512333e87ecb0227b9553c455468f3
andersoneves <anderson.pereira.neves1@gmail.com>
2017-09-18 10:01:24 -0400

    classe BD correção de segurança

--- bd.class.php
+++ bd.class.php
@@ -6,29 +6,49 @@ class bd{
 	private $host = 'localhost';
 
 	//usuario
-	private $usuario = 'root';
+	private $usuario = 'siaf';
 
 	//senha
 	private $senha = '123456';
 
 	//banco de dados
-	private $bd = 'tcc';
+	private $bd = 'siaf';
+
+	private $conn=NULL;
+	
 
 	public function conecta_mysql(){
 
 		//cria conexao
-		$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->bd);
+		$this->conn = mysqli_connect($this->host, $this->usuario, $this->senha, $this->bd);
 
 		//ajusta charset de comunicação entre aplicação e bd
-		mysqli_set_charset($con, 'utf8');
+		mysqli_set_charset($this->conn, 'utf8');
 
 		//verificar se houve erro de conexão
 		if(mysqli_connect_errno()){
 			echo 'Erro ao tentar se conectar com o BD MySQL: ' .mysqli_connect_error();
 		}
 
-		return $con; 
+		return $this->conn; 
 	}
+	
+	function __construct()
+	{
+		$this->conecta_mysql();
+	}
+
+	public function exec($query,$type,$param){
+		$stmt=$this->conn->prepare($query);
+		$stmt->bind_param($type,...$param);
+		$stmt->execute();
+		return $stmt->get_result();
+	}
+
+	public function __destruct(){
+		mysqli_close($this->conn);
+	}
+
 }
 
 ?>
